b7cc8f523cdd9986b5d14bcdd1a37fac0aca0e11
xy <paul2487rqww95@gmail.com>
2025-03-16 09:36:13 +0800

    Test

--- src/main/java/me/xingyan/suggestionBook/Main.java
+++ src/main/java/me/xingyan/suggestionBook/Main.java
@@ -7,6 +7,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Suggestion Book Plugin Enabled");
 
     }
 
